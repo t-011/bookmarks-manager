faae7f061693114644125eae4acbba1af7bd68f6
Tariq Al Farsi <tariq.s.s.alfarsi@gmail.com>
2026-05-06 00:21:01 +1000

    feat(dependencies): decode jwt

---
+++ api/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+security = HTTPBearer()
+jwt_secret = os.environ.get("JWT_SECRET")
+
+async def get_current_user(creds: HTTPAuthorizationCredentials = Depends(security)):
+    creds.credentials
+    try:
+        payload = jwt.decode(token, jwt_secret, algorithms=["HS256"])
+        return payload
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+        
